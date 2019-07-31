8b52e2224564c20bc5a521c3deccf39b938125b4
Xavier Tremel <xavier.tremel@gmail.com>
2019-07-31 10:52:52 +0200

    Edit image

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,11 +1,34 @@
-import React from 'react'
-import { Modal } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Modal, Form, Button } from 'semantic-ui-react'
 
 const Image = (props) => {
+  const [values, setValues] = useState({
+    title: props.image.title,
+    Author: props.image.Author,
+  })
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  }
+
   return (
     <Modal open={props.open} onClose={props.onClose}>
-      <h1>Image</h1>
-      <p>{props.image.title}</p>
+      <Modal.Header>Image</Modal.Header>
+      <Modal.Content image>
+        <Modal.Description>
+          <Form onSubmit={() => props.onSubmit({ ...props.image, ...values })}>
+            <Form.Field>
+              <label>Title</label>
+              <input placeholder='Title' value={values.title} onChange={handleChange('title')} />
+            </Form.Field>
+            <Form.Field>
+              <label>Author</label>
+              <input placeholder='Author' value={values.Author} onChange={handleChange('Author')} />
+            </Form.Field>
+            <Button type='submit'>Submit</Button>
+          </Form>
+        </Modal.Description>
+      </Modal.Content>
     </Modal>
   )
 }
--- src/Screens/Images.js
+++ src/Screens/Images.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-
 import { Table } from 'semantic-ui-react'
+
 import Image from '../Components/Image'
 
 const Images = () => {
@@ -17,6 +17,11 @@ const Images = () => {
       .catch(error => console.error(error))
   }, [])
 
+  const handleSubmit = (updatedImage) => {
+    //TODO update the image via the API
+    setImages(images.map(image => image._id === updatedImage._id ? updatedImage : image))
+    setSelectedImage(null)
+  }
 
   return (
     <React.Fragment>
@@ -26,16 +31,27 @@ const Images = () => {
         : <Table>
             <Table.Header>
               <Table.Row>
-                <Table.HeaderCell>
-                  Title
-                </Table.HeaderCell>
+                {['Title', 'Author', 'Description', 'Date'].map((header, index) =>
+                  <Table.HeaderCell key={index}>
+                    {header}
+                  </Table.HeaderCell>
+                )}
               </Table.Row>
             </Table.Header>
             <Table.Body>
               {images.map(image =>
-                <Table.Row key={image._id} onClick={() => setSelectedImage(image)}>
+                <Table.Row key={image._id} onClick={() => setSelectedImage(image)} style={{ cursor: 'pointer' }}>
                   <Table.Cell>
                     {image.title}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {image.Author}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {image.description}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {image.created}
                   </Table.Cell>
                 </Table.Row>
               )}
@@ -47,6 +63,7 @@ const Images = () => {
           open={!!selectedImage}
           onClose={() => setSelectedImage(null)}
           image={selectedImage}
+          onSubmit={(updatedImage) => handleSubmit(updatedImage)}
         />
       }
     </React.Fragment>
